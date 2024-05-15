320d1f58f7de51f75454cbbb0d67845b1937d187
ocamper <danielocampolozano@gmail.com>
2024-05-14 22:48:20 -0500

    Fin de nueva info, pantalla de inicio
    Se terminó de agregar la info de los animales y sus interacciones. Los animales estan completos, solo faltan sus patrones de movimiento. También se añadió una pantalla principal, tal vez pueda usarla si me alcanza el tiempo (eso espero)

---
+++ Exploramar proyecto/Assets/Scripts/titleScreenManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class titleScreenManager : MonoBehaviour
+{
+    
+    public void ButtonSelected()
+    {
+        Debug.Log(gameObject + " was selected");
+    }
+
+}
